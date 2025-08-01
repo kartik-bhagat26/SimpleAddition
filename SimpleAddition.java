9167e70ac7a8fa12c69016230e6dee599f45c423
kartik bhagat <kartikbhagat1611@gmail.com>
2025-08-01 17:32:00 +0530

    Add space

--- SimpleAddition.java
+++ SimpleAddition.java
@@ -4,7 +4,7 @@ class SimpleAddition
         int num1 = 60;
         int num2 = 9;
         int num3 = 10;
-        int sum = num1 + num2+num3;
+        int sum = num1 + num2+ num3;
 
         System.out.println("Sum of " + num1 + " and " + num2 + "and" + num3 + " is: " + sum);
     }
