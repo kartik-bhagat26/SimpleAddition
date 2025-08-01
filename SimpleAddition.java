b5391b99e4290703c1ab324d76929ebe935b93f0
kartik bhagat <kartikbhagat1611@gmail.com>
2025-08-01 17:18:03 +0530

    Add Java addition program

---
+++ SimpleAddition.java
@@ -0,0 +1,11 @@
+class SimpleAddition 
+{
+    public static void main(String[] args) {
+        int num1 = 60;
+        int num2 = 9;
+        int num3 = 10;
+        int sum = num1 + num2+num3;
+
+        System.out.println("Sum of " + num1 + " and " + num2 + "and" + num3 + " is: " + sum);
+    }
+}
